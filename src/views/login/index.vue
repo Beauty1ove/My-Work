47210b1ca83d112da3d7576a5d2b122f18d9f57f
Beauty1ove <71165656@qq.com>
2019-07-09 22:06:19 +0800

    路由组件：登陆页配制成功

---
+++ src/views/login/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="login-container">
+    <el-card class="login-box">
+      <img src="../../assets/images/logo_index.png" alt />
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped lang="less">
+.login-container {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background: url("../../assets/images/login_bg.jpg") no-repeat center / cover;
+  .login-box {
+    width: 400px;
+    height: 300px;
+    position: absolute;
+  }
+}
+</style>
