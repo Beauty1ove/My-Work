5c4b2c067b540fe6757076e6e51b954731a2db03
Beauty1ove <71165656@qq.com>
2019-07-09 18:07:49 +0800

    路由模块配制

--- src/main.js
+++ src/main.js
@@ -3,10 +3,14 @@ import App from './App.vue'
 
 import ElementUi from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+
+import router from './router/index.js'
+
 Vue.use(ElementUi)
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
