3fdecb83fa2626efd205bda85fd92dcad0ac441b
Beauty1ove <71165656@qq.com>
2019-07-14 09:22:51 +0800

    请求拦截，除登陆外每次请求都需要token、响应拦截，保持token只在有效期可用

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,4 +3,24 @@ import axios from 'axios'
 const instance = axios.create({
   baseURL: 'http://ttapi.research.itcast.cn/mp/v1_0'
 })
+
+instance.interceptors.request.use(config => {
+  const user = window.sessionStorage.getItem('hm74-toutiao')
+  if (user) {
+    config.headers = {
+      Authorization: 'Bearer ' + JSON.parse(user).token
+    }
+  }
+  return config
+}, error => {
+  return Promise.reject(error)
+})
+
+instance.interceptors.response.use(response => response, error => {
+  if (error.response.status === 401) {
+    location.hash = '#/login'
+  }
+  return Promise.reject(error)
+})
+
 export default instance
