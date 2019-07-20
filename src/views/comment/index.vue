c3dc6285259b1a8d7faa643035eadd0d67cb344f
Beauty1ove <71165656@qq.com>
2019-07-20 21:23:51 +0800

    评论管理功能完成

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -6,14 +6,26 @@
       </div>
       <!-- 列表 -->
       <el-table :data="comments">
-        <el-table-column label="标题" width="400"></el-table-column>
-        <el-table-column label="总评论数"></el-table-column>
-        <el-table-column label="粉丝评论数"></el-table-column>
-        <el-table-column label="状态"></el-table-column>
-        <el-table-column label="操作" width="120"></el-table-column>
+        <el-table-column label="标题" width="400" prop="title"></el-table-column>
+        <el-table-column label="总评论数" prop="total_comment_count"></el-table-column>
+        <el-table-column label="粉丝评论数" prop="fans_comment_count"></el-table-column>
+        <el-table-column label="状态">
+          <template slot-scope="scope">{{scope.row.comment_status?'正常':'关闭'}}</template>
+        </el-table-column>
+        <el-table-column label="操作" width="120">
+          <template slot-scope="scope">
+            <el-button
+              v-if="!scope.row.comment_status"
+              type="success"
+              size="mini"
+              @click="toggle(scope.row)"
+            >打开评论</el-button>
+            <el-button v-else type="danger" size="mini" @click="toggle(scope.row)">关闭评论</el-button>
+          </template>
+        </el-table-column>
       </el-table>
       <!-- 分页 -->
-      <el-pagination style="margin-top:20px" background layout="prev, pager, next" :total="1000"></el-pagination>
+      <el-pagination style="margin-top:20px" background layout="prev, pager, next" :total="total"></el-pagination>
     </el-card>
   </div>
 </template>
@@ -23,7 +35,43 @@ export default {
   data () {
     return {
       // 评论信息
-      comments: []
+      comments: [],
+      // 参数
+      reqParams: {
+        page: 1,
+        per_page: 20,
+        response_type: 'comment'
+      },
+      // 总条数
+      total: 0
+    }
+  },
+  created () {
+    this.getComments()
+  },
+  methods: {
+    async getComments () {
+      const {
+        data: { data }
+      } = await this.$http.get('/articles', { params: this.reqParams })
+      this.comments = data.results
+      this.total = data.total_count
+    },
+    toggle (row) {
+      const text1 = '您确认要打开该文章的评论功能吗？'
+      const text2 = '关闭评论功能后用户将无法对该文章进行评论，您确认要关闭该文章的评论功能吗？'
+      this.$confirm(row.comment_status ? text2 : text1, '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(async () => {
+        const {
+          data: { data }
+        } = await this.$http.put('/comments/status?article_id=' + row.id, {
+          allow_comment: !row.comment_status
+        })
+        row.comment_status = data.allow_comment
+      }).catch(() => {})
     }
   }
 }
